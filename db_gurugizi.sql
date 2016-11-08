d623872d9a3944d267bc2ca320c52f342ded47c2
Daniel Sugiarto Setyabudi <danielsugiarto89@gmail.com>
2016-11-08 21:30:00 +0700

    ini db yang baru

--- db_gurugizi.sql
+++ db_gurugizi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 01 Nov 2016 pada 13.36
+-- Generation Time: 08 Nov 2016 pada 15.22
 -- Versi Server: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -23,33 +23,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `nutritionist`
+--
+
+CREATE TABLE `nutritionist` (
+  `email` varchar(150) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(300) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `notelp` varchar(15) NOT NULL,
+  `domisili` varchar(100) NOT NULL,
+  `foto_profil` varchar(100) NOT NULL,
+  `file_cv` varchar(100) NOT NULL,
+  `file_transkrip_nilai` varchar(100) NOT NULL,
+  `file_ijazah` varchar(100) NOT NULL,
+  `file_str` varchar(100) NOT NULL,
+  `keterangan` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `pasien`
 --
 
 CREATE TABLE `pasien` (
-  `nickname` varchar(100) NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `gender` varchar(1) NOT NULL,
-  `birthday` date NOT NULL,
-  `kota` varchar(50) NOT NULL,
-  `keterangan` varchar(1000) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(300) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `notelp` varchar(15) NOT NULL,
+  `domisili` varchar(100) NOT NULL,
   `berat_badan` int(11) NOT NULL,
-  `tinggi_badan` int(11) NOT NULL
+  `tinggi_badan` int(11) NOT NULL,
+  `foto_profil` varchar(100) NOT NULL,
+  `keterangan` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `pasien`
 --
 
-INSERT INTO `pasien` (`nickname`, `fullname`, `email`, `password`, `gender`, `birthday`, `kota`, `keterangan`, `berat_badan`, `tinggi_badan`) VALUES
-('Budi', 'Budi Salim', 'budisalim@gmail.com', '123456', 'L', '1996-10-16', 'Surabaya', '', 0, 0),
-('Daniel', 'Daniel Sugiarto Setyabudi', 'danielsugiarto89@gmail.com', '123456', 'L', '1996-07-12', 'Surabaya', 'Saya seorang staff IT berbadan gemuk.', 100, 165);
+INSERT INTO `pasien` (`email`, `password`, `nama`, `tanggal_lahir`, `jenis_kelamin`, `notelp`, `domisili`, `berat_badan`, `tinggi_badan`, `foto_profil`, `keterangan`) VALUES
+('danielsugiarto89@gmail.com', '123456', 'Daniel Sugiarto Setyabudi', '1996-07-12', 'L', '', 'Surabaya', 100, 165, '', 'Saya seorang staff IT berbadan gemuk.');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `nutritionist`
+--
+ALTER TABLE `nutritionist`
+  ADD PRIMARY KEY (`email`);
 
 --
 -- Indexes for table `pasien`
